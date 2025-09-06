45b02089db5c6c124aa237b3884c453ca91cffe3
FELIX-OFC <felixofc3@gmail.com>
2025-09-06 18:11:26 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -483,14 +483,14 @@ global.dfail = (type, m, conn, usedPrefix, command) => {
 
     const msg = {
         rowner: '「🩵」Este comando solo puede ser usado por mi creador.\n\n> Félix Manuel',
-        owner: '「💎」Este comando solo puede ser usado por mi creador',
+        owner: '「💎」Este comando solo puede ser usado por Duarte',
         premium: '「🩵」 Este comando solo puede ser usado por los usuarios premiums.',
         botprem: '「💥」Este comando solo está disponible para bots premium.',
         private: '「💎」Este comando solo puede ser usado en chats privados.',
         admin: '「🩵」Este comando solo puede ser usado por admins.',
         botAdmin: '「💎」Para usar este comando, debo ser admin del grupo.',
-        unreg: '「🩵」¡Hey! no estas registrado, registrate para usar mis comandos\n\n/Reg nombre.edad\n\n! Ejemplo: _/Reg Félix.14_',
-        restrict: '「💎」Este comando fue desactivado por mi Creador\n\n> Félix Manuel.'
+        unreg: '「🩵」¡Hey! no estas registrado, registrate para usar mis comandos\n\n/Reg nombre.edad\n\n! Ejemplo: _/Reg Duarte.14_',
+        restrict: '「💎」Este comando fue desactivado por mi Creador\n\n> Duarte.'
     }[type];
 
     if (msg)
