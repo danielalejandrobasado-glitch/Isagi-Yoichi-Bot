9f4bef40fd356534b5280fb95ecc36b5413dba21
danielalejandrobasado-glitch <danielalejandrobasado@gmail.com>
2025-09-07 16:16:47 -0500

    Update Convertidor•tts.js

--- plugins/Convertidor•tts.js
+++ plugins/Convertidor•tts.js
@@ -16,7 +16,7 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
   } catch (e) {
     m.reply(e + '');
     text = args.join(' ');
-    if (!text) return conn.reply(m.chat, `🚩 *Te Faltó Un Texto*\n\nEjemplo:\n${usedPrefix + command} Hi maki`, m, rcanal);
+    if (!text) return conn.reply(m.chat, `🚩 *Te Faltó Un Texto*\n\nEjemplo:\n${usedPrefix + command} Hi Isagi`, m, rcanal);
     res = await tts(text, defaultLang);
   } finally {
     if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true);
